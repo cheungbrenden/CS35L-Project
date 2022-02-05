916d7297d4b12c88da7852c87c6cc7375ec1f13a
Solia Valentine <soliavalentine@gmail.com>
2022-02-04 22:48:13 -0800

    first button commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import ButtonComponent from './Components/Button.js';
+import Button from './Components/Button.js'
 function App() {
+  return (
+    <ButtonComponent></ButtonComponent>
+  );
+}
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +26,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
