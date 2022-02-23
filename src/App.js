61e3ad9413fd9eb1fe438203d8e07855b2bdb52a
Laura <lauralu201@g.ucla.edu>
2022-02-23 14:46:41 -0800

    theme is done and font has been added :)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Sides} from './pages';
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
       <header className="App-header">
         <Sides/>
       </header>
