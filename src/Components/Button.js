d23171123db63ae604168541648147dd8ec84371
Solia Valentine <soliavalentine@gmail.com>
2022-02-23 15:20:39 -0800

    nonpopover button component with props

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import Button from '@mui/material/Button';
-import Popover from '@mui/material/Popover';
-
 export default function ButtonComponent(props){
     const myClass = `button ${props.type}`
     const myId = `button ${props.type}`
@@ -10,52 +8,7 @@ export default function ButtonComponent(props){
         {maxWidth: '300px', maxHeight: '300px', minWidth: '300px', minHeight: '300px'}}
         variant="contained" className={myClass} 
         id={myId} onClick={props.handleClick}>{props.children}
-        onMouseOver={props.handleMouseOver}
+
         </Button>
     )
     }
-    /*export default function MouseOverPopover() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-
-  return (
-    <div>
-      <Typography
-        aria-owns={open ? 'mouse-over-popover' : undefined}
-        aria-haspopup="true"
-        onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}
-      >
-        Hover with a Popover.
-      </Typography>
-      <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: 'none',
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus
-      >
-        <Typography sx={{ p: 1 }}>I use Popover.</Typography>
-      </Popover>
-    </div>
-  ); */
