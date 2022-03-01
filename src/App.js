345748aea6804f2ece8ec83080139e31fbbe04c0
Laura <lauralu201@g.ucla.edu>
2022-02-28 19:01:33 -0800

    Deleted useless pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { ConfirmOrder, Login, Sides, History } from "./pages";
+import { ConfirmOrder,  History } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,10 +10,6 @@ function App() {
         <Routes>
           {/* Default path goes to WhatMajor page */}
           <Route exact path="/confirm" element={<ConfirmOrder/>} />
-          {/* <Route exact path="/minors" element={<WhatMajor majmin={"minors"} />} /> */}
-          <Route exact path="/login" element={<Login/>} />
-          {/* <Route exact path="/courses" element={<EnterCourses courses={courses} />} /> */}
-          <Route exact path="/sides" element={<Sides/>} />
           <Route exact path="/history" element={<History/>} />
         </Routes>
       </Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-export { default as Sides } from './Sides';
-export { default as Login } from './Login';
 export { default as History } from './History';
 export { default as ConfirmOrder } from './ConfirmOrder';
-export { default as OrderHistory } from './OrderHistory';
 
 
 
