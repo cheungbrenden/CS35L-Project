60ec2b07fe0b067b8dbe9457ae87fbbb853ae43c
ZhangPowa <78633730+ZhangPowa@users.noreply.github.com>
2022-03-01 00:07:23 -0800

    fixed backend config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,9 +17,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
 
 const signInWithEmailAndPassword = async (email, password) => {
   try {
