d59ce32ffef8b09f139247ca239111ebbcb2a046
ZhangPowa <78633730+ZhangPowa@users.noreply.github.com>
2022-03-02 01:04:32 -0800

    login works now, but login state is stored
    also still need to test for wrong login

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
-// import { getAuth } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,9 +18,17 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const auth = getAuth();
-const db = getFirestore();
+const auth = getAuth(app);
+const db = getFirestore(app);
 
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
 // const signInWithEmailAndPassword = async (email, password) => {
 //   try {
 //     await auth.signInWithEmailAndPassword(email, password);
@@ -55,4 +63,4 @@ const db = getFirestore();
 // };
 
 // export default app;
-export { db };
+export { auth, db, logInWithEmailAndPassword };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, logInWithEmailAndPassword } from "../firebase/config";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { makeStyles } from '@mui/styles';
-import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
 // import  signInWithEmailAndPassword } from 'firebase/auth';
-import { useState} from 'react';
-import { useNavigate } from "react-router-dom";
 const UseStyles = makeStyles((theme) => ({
     layout: {
       display: 'flex',
@@ -39,29 +39,15 @@ function Login() {
     console.log ("login")
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [user, loading, error] = useAuthState(auth);
     const navigate = useNavigate();
-    const signIn = () => {
-      const auth = getAuth();
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed in 
-          const user = userCredential.user;
-          // ...
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-        });
-      navigate("/home");
-    };
-    // useEffect(() => {
-    //   // if (loading) {
-    //   //   return;
-    //   }
-    //   // if (user) {
-    //   //     history.replace("/dashboard");
-    //   // }
-    // }, [user, loading]);
+    useEffect(() => {
+      if (loading) {
+        // maybe trigger a loading screen
+        return;
+      }
+      if (user) navigate("/home");
+    },[user, loading]);
     return (
       <div className={login.layout}>
         <div className = {login.title}>
@@ -72,17 +58,21 @@ function Login() {
         </div>
        <p>Email</p>
        <input
-       value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}></input>
+        type="text"
+        className="login__textBox"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="E-mail Address"
+       ></input>
        <p>Password</p>
        <input 
-       value={password}
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}></input>
-       <button onClick={signIn}>Sign In
+        type="password"
+        className="login__textBox"
+        value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         placeholder="Password">
+       </input>
+       <button onClick={() =>logInWithEmailAndPassword(email, password)}>Sign In
        </button>
       </div>
     );
