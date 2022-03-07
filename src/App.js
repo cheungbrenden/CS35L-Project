c42d9f63e0e903d2d3245cf73367f1630a182819
Laura <lauralu201@g.ucla.edu>
2022-03-07 00:17:35 -0800

    i still need to fix the filter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { ConfirmOrder, Login, Sides, History } from "./pages";
+import {History } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,12 +8,7 @@ function App() {
     <div className="App">
        <Router>
         <Routes>
-          {/* Default path goes to WhatMajor page */}
-          <Route exact path="/confirm" element={<ConfirmOrder/>} />
-          {/* <Route exact path="/minors" element={<WhatMajor majmin={"minors"} />} /> */}
-          <Route exact path="/login" element={<Login/>} />
-          {/* <Route exact path="/courses" element={<EnterCourses courses={courses} />} /> */}
-          <Route exact path="/sides" element={<Sides/>} />
+         
           <Route exact path="/history" element={<History/>} />
         </Routes>
       </Router>
--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -4,6 +4,10 @@ import { makeStyles } from '@mui/styles';
 import { db } from '../firebase/config';
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const UseStyles = makeStyles((theme) => ({
     layout: {
@@ -12,22 +16,48 @@ const UseStyles = makeStyles((theme) => ({
       flexDirection: 'column',
       width: '100vw',
     },
-   
+
+    output: {
+      font: theme.font.output,
+      color: theme.color.black,
+      textAlign: 'center',
+    },
+
     title: {
       font: theme.font.title,
       color: theme.color.black,
-      textAlign: 'center',
-      margin: '0 0 0 0.9rem',
-      marginTop: '20.4rem',
-      fontWeight: 'bold',
+      margin: '0 0 0 0 rem',
+      marginTop: '2.4rem',
     },
-  
+
+    entree: {
+      font: theme.font.output,
+      color: theme.color.white, 
+      background: theme.color.brown,
+      textAlign: 'center',
+      padding: '0.5rem',
+      margin: '1 rem',
+    },
+
+    details: {
+      font: theme.font.output,
+      color: theme.color.black, 
+      background: theme.color.background,
+      margin: '1 rem',
+      width: '20rem',
+      padding: '0.3rem',
+      borderColor: theme.color.black,
+      border: '0.1rem',
+      textAlign: 'center',
+    }
   }));
 
 function History() {
     const history = UseStyles();
     const [users, setUsers] = useState ([]); 
     const userCollectionRef = collection(db, 'Orders'); 
+    const [entree, setEntree] = React.useState("");
+    const [order, setOrder] = useState('Start')
 
     useEffect(() => {
         const getUsers = async () => {
@@ -38,16 +68,103 @@ function History() {
         getUsers();
     }, [])
 
+    const filter = (event) => {
+      console.log ("event", event.target.value)
+      setEntree(event.target.value);
+      
+      if (event.target.value === 'All'){
+        setOrder ('Start')
+      }
+      
+      else {
+        setOrder (event.target.value);
+      }
+
+      const asArray = Object.entries(users);
+      console.log (asArray)
+
+      const filtered = asArray.filter(user => (user[1].Entree === event.target.value));
+      setUsers (filtered)
+
+    }
+
+    function usersmap () {
+      
+      return (
+         users.map ((user) => {
+        return (
+          <div className = {history.details}>
+           {print(user)}
+          </div>
+        );
+      })    
+      )
+    }
+
+    function print(array) 
+    {
+      var str = JSON.stringify(array, null);
+      console.log ("str", str)
+      str = str.replaceAll('"', '')
+      str = str.replaceAll(':', ': ')
+      str = str.replaceAll('{', '')
+      str = str.replaceAll('}', '')
+
+      var MyArray = str.split(',');
+      MyArray = MyArray.sort();
+      
+      for (var i = 0; i<MyArray.length; i++){
+        if (MyArray[i].startsWith('Entree')){
+          var foundIdx = i;
+        }
+      }
+      console.log("foundIDx", foundIdx)
+      MyArray.unshift(MyArray[foundIdx])
+      MyArray.splice(foundIdx+1,1)
+      var details = MyArray.slice(1, MyArray.length)
+      console.log ("details", details)
+
+      return (
+        <div>
+          Hello
+        </div>
+        // <div className = {history.output}>
+        //   <div className = {history.entree}> 
+        //     {
+        //       MyArray[0]
+        //     }
+        //   </div>
+        //   <div>
+        //   {
+        //     details.map((item) => {
+        //     return (<div className = {history.details}>{item}</div>);
+        //   })}
+        //   </div>
+        // </div>
+      );
+    }
+
+
     return (
-      <div className={history.layout}>
-        Your past orders!
-          {users.map ((user) => {
-            return (
-              <div>
-                <h1> Entree: {user.Entree} </h1>
-              </div>
-            );
-          })}
+      <div className = {history.layout}>
+        <div className = {history.title}>Your past orders! </div>
+         <FormControl sx={{ m:3, minWidth: 500 }}>
+        <InputLabel id="demo-simple-select">Entree </InputLabel>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={entree}
+          onChange={filter}
+          autoWidth
+          label="Hello"
+        >
+          <MenuItem value={"All"}>All</MenuItem>
+          <MenuItem value={"Pizza"}>Pizza</MenuItem>
+          <MenuItem value={"Sandwich"}>Sandwich</MenuItem>
+        </Select>
+      </FormControl>
+      {order === 'Start' && (usersmap())}
+      {order === 'Pizza' && (usersmap())}
       </div>
     );
 
