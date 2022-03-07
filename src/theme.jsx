c55e18cc6aab001325db105bbe66ab86318c4bf2
Laura <lauralu201@g.ucla.edu>
2022-03-06 21:39:48 -0800

    my bad i was working on history on master

--- src/theme.jsx
+++ src/theme.jsx
@@ -5,11 +5,14 @@ import "@fontsource/solway";
 const theme = createTheme({
   font: {
     title: 'normal 500 4.5rem/4.5rem "Solway"',
+    output: 'normal 100 1rem/1rem "Solway"',
   },
 
   color: {
     black: "#000000",
     background: "#FDF9F9",
+    brown: "#594A47",
+    white: "#FFFFFF",
   }
 });
 
