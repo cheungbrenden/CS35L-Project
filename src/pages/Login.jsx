27bc8257e76716461c1003ff3a20b341d7c5e9d0
ZhangPowa <78633730+ZhangPowa@users.noreply.github.com>
2022-03-07 17:46:24 -0800

    added new paths

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -84,7 +84,7 @@ function Login() {
         // maybe trigger a loading screen
         return;
       }
-      if (user) navigate("/dashboard");
+      if (user) navigate("/Sausage");
     },[user, loading]);
     return (
       <div className={login.layout}>
--- src/pages/Sausage.jsx
+++ src/pages/Sausage.jsx
@@ -71,7 +71,7 @@ const UseStyles = makeStyles((theme) => ({
         setErrorMessage('Please select an option');  
       }
   else{
-    let path = `newPath`; //change to correct path
+    let path = `/postorder`; //change to correct path
   navigate(path);}
   }
     const sausage = UseStyles();
