ab1748dbf3aaef98a9c880733c8d70757954f1b3
ZhangPowa <78633730+ZhangPowa@users.noreply.github.com>
2022-03-08 18:10:06 -0800

    played around with dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -83,17 +83,17 @@ function Dashboard() {
       console.log(error);
     }
   }
-// {/* //   const fetchUserName = async () => { */}
-// //     try {
-// //       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
-// //       const doc = await getDocs(q);
-// //       const data = doc.docs[0].data();
-// //       setName(data.name);
-// //     } catch (err) {
-// //       console.error(err);
-// //       alert("An error occured while fetching user data");
-// //     }
-// //   };
+  // const fetchUserName = async () => {    
+  //   try {
+  //     const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+  //     const doc = await getDocs(q);
+  //     const data = doc.docs[0].data();
+  //     setName(data.name);
+  //   } catch (err) {
+  //     console.error(err);
+  //     alert("An error occured while fetching user data");
+  //   }
+  // };
   useEffect(() => {
     adduserid();
     getFavOrders();
