daf267927052d53a3cb80c65b5e754fa4539fd2b
cheungbrenden <cheungbrenden@gmail.com>
2022-03-08 19:55:11 -0800

    Changes:  Added Pizza and Start Order pages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ import {
   SaladToppings, 
   SaladGreens, 
   SaladProteins, 
-  SaladDressings
+  SaladDressings,
+  StartOrder,
+    PizzaSauce,
+    PizzaCheese,
+    PizzaAddons,
+    PizzaToppings,
 } from "./pages";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
@@ -26,18 +31,20 @@ function App() {
           <Route exact path="/history" element={<History/>} />
           <Route exact path="/sausage" element={<Sausage/>} />
           <Route exact path="/european" element={<European/>} />
-          {/* <Route exact path="/minors" element={<WhatMajor majmin={"minors"} />} /> */}
           <Route exact path="/login" element={<Login/>} />
-          {/* <Route exact path="/courses" element={<EnterCourses courses={courses} />} /> */}
           <Route exact path="/SaladToppings" element={<SaladToppings/>} />
           <Route exact path="/SaladGreens" element={<SaladGreens/>} />
           <Route exact path="/SaladProteins" element={<SaladProteins/>} />
           <Route exact path="/SaladDressings" element={<SaladDressings/>} />
-          {/* <Route exact path="/minors" element={<WhatMajor majmin={"minors"} />} /> */}
-          {/* <Route exact path="/courses" element={<EnterCourses courses={courses} />} /> */}
           <Route exact path="/home" element={<Home />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/postorder" element={<PostOrder />} />
+          <Route exact path="/StartOrder" element={<StartOrder />} />
+          <Route exact path="/PizzaToppings" element={<PizzaToppings />} />
+          <Route exact path="/PizzaSauce" element={<PizzaSauce />} />
+          <Route exact path="/PizzaCheese" element={<PizzaCheese />} />
+          <Route exact path="/PizzaAddons" element={<PizzaAddons />} />
+
         </Routes>
       </Router>
     </div>
